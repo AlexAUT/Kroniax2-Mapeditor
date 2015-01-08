add910c13e0077b0360a383d2ae974e681217276
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2015-01-08 16:24:13 +0100

    Added main

---
+++ include/main.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////
+//
+// Kroniax2-Mapeditor
+// Copyright (C) 2015 Alexander Weinrauch (alexander.weinrauch@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// License information at https://github.com/AlexAUT/Kroniax2-Mapeditor/blob/master/LICENSE
+//
+////////////////////////////////////////////////////////////
+
+#include "application.hpp"
+
+int main()
+{
+  Application app;
+
+  return app.run();
+}
