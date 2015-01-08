eb7e0417ad569a518d0dbd64dd8ab218f09d4acb
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2015-01-08 16:24:40 +0100

    Fixed copy and paste error in log.cpp

--- include/aw/utilities/log.cpp
+++ include/aw/utilities/log.cpp
@@ -23,14 +23,4 @@ namespace aw
   {
     mLogFile.open(filePath.c_str(), std::ios::app);
   }
-
-  template<class T>
-  void Log::write(const T &message)
-  {
-    std::time_t t = std::time(nullptr);
-
-    std::cout << message << std::endl;
-    if (mLogFile.is_open())
-      mLogFile << std::put_time(std::localtime(&t), "%c: ") << message << std::endl;
-  }
 }
