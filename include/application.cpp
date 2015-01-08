45a965696fec670bea02fce0ccdb284b7bd763af
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2015-01-08 19:30:40 +0100

    Added Copyright text to source files

--- include/application.cpp
+++ include/application.cpp
@@ -1,3 +1,15 @@
+////////////////////////////////////////////////////////////
+//
+// Kroniax2-Mapeditor
+// Copyright (C) 2015 Alexander Weinrauch (alexander.weinrauch@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// License information at https://github.com/AlexAUT/Kroniax2-Mapeditor/blob/master/LICENSE
+//
+////////////////////////////////////////////////////////////
+
 #include "application.hpp"
 
 #include "aw/utilities/log.hpp"
--- include/aw/utilities/log.inl
+++ include/aw/utilities/log.inl
@@ -1,3 +1,16 @@
+////////////////////////////////////////////////////////////
+//
+// Kroniax2-Mapeditor
+// Copyright (C) 2015 Alexander Weinrauch (alexander.weinrauch@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// License information at https://github.com/AlexAUT/Kroniax2-Mapeditor/blob/master/LICENSE
+//
+////////////////////////////////////////////////////////////
+
+
 #include <iostream>
 #include <iomanip>
 #include <ctime>
